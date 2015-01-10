7ed3f406195c686c109d40c4b1f8cb16b79ce329
Isaiah J Turner <github.com@isaiahjturner.com>
2015-01-10 03:26:44 -0500

    fic

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+
+// this fixes ios opening web app in safari
+if (("standalone" in window.navigator) && window.navigator.standalone) {
+      // For iOS Apps
+      $('a').on('click', function(e){
+        e.preventDefault();
+        var new_location = $(this).attr('href');
+        if (new_location != undefined && new_location.substr(0, 1) != '#' && $(this).attr('data-method') == undefined && $(this).attr('target') != "_blank"){
+          window.location = new_location;
+        }
+      });
+    }
