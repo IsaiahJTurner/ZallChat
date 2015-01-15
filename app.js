489809af6bab770b2fe35d5c55847818c66de0df
Isaiah J Turner <github.com@isaiahjturner.com>
2015-01-14 19:33:44 -0500

    gifx

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(expressLayouts);
 app.use(cookieParser());
 // app.use(express.logger('tiny'))
 if (!process.env.EB) 
-	app.use(express.static(path.join(__dirname, 'public')));
+	app.use("/static", express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
 	res.header("X-powered-by", "@IsaiahJTurner")
 	next()
